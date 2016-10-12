58723936f742d7ef10cf9dc706c3f73919297963
Kohei Tabata <nerd0geek1@gmail.com>
2016-10-12 23:37:15 +0900

    organize project packaging

---
+++ Multipeer-Connectivity-iOS/classes/viewcontrollers/TopViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TopViewController.swift
+//  Multipeer-Connectivity-iOS
+//
+//  Created by Kohei Tabata on 10/12/16.
+//  Copyright © 2016 Kohei Tabata. All rights reserved.
+//
+
+import UIKit
+
+class TopViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
