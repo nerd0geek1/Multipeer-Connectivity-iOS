8f6c21c484599a392ec8e6c6e29ffef87be2e3f9
Kohei Tabata <nerd0geek1@gmail.com>
2016-10-12 23:32:13 +0900

    add project

---
+++ Multipeer-Connectivity-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multipeer-Connectivity-iOS
+//
+//  Created by Kohei Tabata on 10/12/16.
+//  Copyright © 2016 Kohei Tabata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
