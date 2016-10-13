d42901bc56f050570dbcbaa5bfe7ae829f621690
Kohei Tabata <nerd0geek1@gmail.com>
2016-10-13 22:36:19 +0900

    add MultipeerBrowser/MultipeerAdvertiser

---
+++ Multipeer-Connectivity-iOS/classes/feature/p2p/MultipeerBrowser.swift
@@ -0,0 +1,13 @@
+//
+//  MultipeerBrowser.swift
+//  Multipeer-Connectivity-iOS
+//
+//  Created by Kohei Tabata on 10/13/16.
+//  Copyright © 2016 Kohei Tabata. All rights reserved.
+//
+
+import Foundation
+
+class MultipeerBrowser {
+
+}
